e64ed5e0fe3b35969f1fc4a9ffb09d3c3ac77445
Nick Muerdter <12112+GUI@users.noreply.github.com>
2026-06-03 16:35:31 -0600

    Rely on kubernetes secrets in deployed environment.

--- db-syncer/config/initializers/vault_env_secrets.rb
+++ db-syncer/config/initializers/vault_env_secrets.rb
@@ -1,1 +1,6 @@
+# Read secrets from main app config.
 VaultEnvSecrets.template_path = Rails.root.join("../config/vault_secrets.json.tmpl")
+
+# Read Vault secrets into environment variables for local development (in
+# production, these will be handled via Kubernetes secrets).
+VaultEnvSecrets.enabled = (ENV["LOAD_VAULT_ENV_SECRETS"] == "true")
