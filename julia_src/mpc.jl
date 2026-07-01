919d30cb7c5026f5c4e71556277e7d122e8a5985
adfarth <adfarthing1@gmail.com>
2026-07-01 08:23:27 -0600

    utf-8

--- julia_src/mpc.jl
+++ julia_src/mpc.jl
@@ -1,4 +1,4 @@
-﻿# REopt®, Copyright (c) Alliance for Sustainable Energy, LLC. See also https://github.com/NatLabRockies/REopt_API/blob/master/LICENSE.
+# REopt®, Copyright (c) Alliance for Sustainable Energy, LLC. See also https://github.com/NatLabRockies/REopt_API/blob/master/LICENSE.
 # ============================================================================
 # MPC (Model Predictive Control) endpoint
 # ----------------------------------------------------------------------------
